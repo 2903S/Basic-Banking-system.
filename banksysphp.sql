b41bcdab1540a88272c6ea2960ab886dd05290b5
2903S <95955483+2903S@users.noreply.github.com>
2021-12-20 12:52:51 +0530

    Update banksysphp.sql

--- banksysphp.sql
+++ banksysphp.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: july 10, 2021 at 04:35 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
